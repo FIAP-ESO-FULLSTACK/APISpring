cd50c8027a6ba4bda66d9574c53a1eb7042a4299
Willian Goncalves <sherlock@MacBook-Air-de-Willian.local>
2025-11-12 17:43:42 -0300

    Created OracleConnection class to connect a Oracle DataBase

--- src/test/java/com/esofiap/globalsolution/GlobalSolutionApplicationTests.java
+++ src/test/java/com/esofiap/globalsolution/GlobalSolutionApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GlobalSolutionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
