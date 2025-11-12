79ce4f1297f30a928586d7d484e1d42be62d6161
Willian Goncalves <sherlock@MacBook-Air-de-Willian.local>
2025-11-12 16:54:39 -0300

    initialized spring

---
+++ src/test/java/com/esofiap/globalsolution/GlobalSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esofiap.globalsolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
